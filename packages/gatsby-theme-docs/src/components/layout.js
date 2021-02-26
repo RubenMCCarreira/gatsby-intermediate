584c6fed3b09f9c29a6b7afa9e385677395c4c77
Rúben Carreira <rubencarreira@Rubens-MBP.lan>
2021-02-26 16:19:16 +0000

    add schema customization

---
+++ packages/gatsby-theme-docs/src/components/layout.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+const Layout = ({ children }) => (
+  <Fragment>
+    <header
+    >
+      gatsby-theme-docs
+    </header>
+    <main>{children}</main>
+  </Fragment>
+);
+
+export default Layout;
